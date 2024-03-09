9634351cf243b6ea47c58f489381cad300cc1674
Chris Evans <chris@psyctc.org>
2024-03-09 15:17:11 +0100

    Much UI/cosmetic work on RCI1

--- apps/RCI1/app.R
+++ apps/RCI1/app.R
@@ -3,71 +3,94 @@ suppressMessages(library(shiny))
 suppressMessages(library(shinyWidgets))
 suppressMessages(library(shiny.telemetry))
 
-### 1. Initialize telemetry with default options (store to a local logfile)
+### telemetry 1. Initialize telemetry with default options (store to a local sqlite database)
 telemetry <- Telemetry$new(app_name = "RCI1",
                            data_storage = DataStorageSQLite$new(db_path = file.path("../../telemetry.sqlite"))) 
 
 # Define UI for application that does the work
 ui <- fluidPage(
   
-  use_telemetry(), # 2. Add necessary Javascript to Shiny
+  use_telemetry(), # telemetry 2. Add necessary Javascript to Shiny
   
   setBackgroundColor("#ffff99"),
-  ### this is from
-  ### https://stackoverflow.com/questions/51298177/how-to-centre-the-titlepanel-in-shiny
-  ### and centers the first title across the whole page by tweaking the css for head blocks
-  tags$head(
-    tags$style(
-      ".title {margin: auto; align: center}"
-    )
-  ),
-  tags$div(class="title", titlePanel("Compute RCI from SD and reliability (and inclusion interval if not 95%)\n\n")),
+  # h1(HTML("<center>Compute RCI from SD and reliability (and inclusion interval if not 95%)</center>")),
+  h1(HTML("Compute RCI from SD and reliability<br>(and inclusion interval if not 95%)"), align = "center"),
   
   # Get input values
   sidebarLayout(
     sidebarPanel(
-      h3("Put your values in here, replacing the existing ones", align="center"),
+      h3("Replace the values here with your own", align="center"),
     numericInput("SD",
-                 "This is your baseline score standard deviation (must be positive))",
+                 "Your baseline score standard deviation (SD)",
                  value = 11.1,
                  min = 0,
                  max = 10^9,
                  width="100%"),
+    helpText("A standard deviation must be numeric and positive!"),
     numericInput("rel",
-                 "Appropriate reliability, generally this should be the Cronbach's alpha from your data but may be a referential value.",
+                 "Reliability",
                  value = .9,
                  min = 0,
                  max = 1,
                  width="100%"),
+    helpText("Generally this will be the Cronbach's alpha from your data but may be a referential value."),
     numericInput("ci",
-                 "Width of inclusion interval of the RCI (usually .95, i.e. 95%)",
+                 "Inclusion interval",
                  value = .95,
                  min = .699999,
                  max = .999,
                  width="100%"),
+    helpText("This is usually .95, i.e. 95% inclusion/coverage."),
     numericInput("dp",
                  "Number of decimal places",
                  value = 2,
                  min = 0,
                  max = 5,
-                 width="100%")
+                 width="100%"),
+    helpText("When you have filled in the above, hit this next button to get the RCI!"),
+    actionButton("compute", "Compute RCI")
   ),
   
   mainPanel(
-    h3("Your input and results",align="center"),
-    verbatimTextOutput("res"),
-    p("This uses the function getRCIfromSDandAlpha() in my CECPfuns R package but the maths is trivial and\n
-       straight out of the original work on the RCI.  If you have the raw item data and n > 20 I recommend you\n
+    
+    # Output: Tabset w/ plot, summary, and table ----
+    tabsetPanel(type = "tabs",
+                tabPanel("Result", 
+                         verbatimTextOutput("res")
+                ),
+                
+                tabPanel("Explanation/information", 
+                         h3("Computation background"),
+                         p(HTML("This uses the function <em>getRCIfromSDandAlpha()</em> in my "),
+                           a("CECPfuns R package",
+                             href = 'https://cecpfuns.psyctc.org/'),
+                           "but the maths is trivial and\n
+       straight out of the original work on the RCI."),
+                         h3("Maths of the RCI "),
+                         p("Ignore this next bit if you don't like equations!"),
+                         p("The full equation is:"),
+                         withMathJax("$$SD*\\sqrt{2}*\\sqrt{1-rel}*qnorm(1 - \\frac{(1 - ci)}{2})$$"),
+                         p("where 'SD' is the SD you input, 'rel' the reliability you input and 'ci' the inclusion interval you wanted, usually .95, i.e. 95% and 'qnorm' is the quantile of the standard Gaussian/Normal distribution for that ci."),
+                         p("So if you asked for the usual 95% interval that simplifies to this:"),
+                         withMathJax("$$SD*\\sqrt{2}*\\sqrt{1-rel}*1.96$$"),
+                         p("because 1.96 is the value of the standard Gaussian that maps to 95% inclusion"),
+                         h3("Recommendations"),
+                         p("If you have the raw item data and n > 20 I recommend you\n
        compute the Cronbach alpha internal reliability for your own data. I will put up an app to compute that\n
        for raw item data when I can.\n\n"),
-    p("App created by Chris Evans",
-      a("PSYCTC.org",href="https://www.psyctc.org/psyctc/about-me/"),
-      "licenced under a ",
-      a("Creative Commons, Attribution Licence-ShareAlike",
-        href="http://creativecommons.org/licenses/by-sa/1.0/"),
-      " Please respect that and put an acknowledgement and link back to here if re-using anything from here."),
-    hr(),
-    includeHTML("https://shiny.psyctc.org/boilerplate.html")
+                         h3("Next please ..."),
+                         p("Unless you are very familiar with it, please now go to the 'Background' tab and read the information there.")
+                ),
+                         
+                tabPanel("Background", 
+                         p("App created by Chris Evans",
+                           a("PSYCTC.org",href="https://www.psyctc.org/psyctc/about-me/"),
+                           "licenced under a ",
+                           a("Creative Commons, Attribution Licence-ShareAlike",
+                             href="http://creativecommons.org/licenses/by-sa/1.0/"),
+                           " Please respect that and put an acknowledgement and link back to here if re-using anything from here."),
+                         includeHTML("https://shiny.psyctc.org/boilerplate.html"))
+    ),
   )
 )
 )
@@ -82,12 +105,8 @@ server <- function(input, output, session) {
     stopApp()
   })
   
-  telemetry$start_session(track_inputs = TRUE, track_values = TRUE) # 3. Track basics and inputs and input values
+  telemetry$start_session(track_inputs = TRUE, track_values = TRUE) # telemetry 3. Track basics and inputs and input values
   
-  ### 
-  ### start with validation functions
-  ### I don't think I actually use any these as I've now used numericInput() to set the ranges
-
   ### 
   ### now the functions adapted from CECPfuns plotCIcorrelation
   ###
@@ -96,27 +115,32 @@ server <- function(input, output, session) {
     ci.perc <- round(100 * ci)
     retText <- paste0("Given:\n",
                       "   SD = ", round(SD, dp), "\n",
-                      "   Reliability = ", round(rel, dp), " and \n",
-                      "   ", ci.perc, "% inclusion interval gives\n",
-                      "   RCI = ", round(RCI, dp), "\n\n")
+                      "   Reliability = ", round(rel, dp), " and ", ci.perc, "% inclusion interval gives\n",
+                      "   RCI = ", round(RCI, dp), "\n\n",
+                      "So given that SD, and reliability, you would expect ",
+                      ci.perc,
+                      "% of your data to have shown absolute change\n (increase or decrease), of less than or equal to ",
+                      round(RCI, dp), 
+                      " were change simply down to measurement unreliability\n i.e. there being no 'real' change happening.\n\n",
+                      "Go to 'Explanation/information' tab for more explanation about the RCI.")
     return(retText)
   }
   
-  output$res <- renderText({
-    # validate(
-    #   need(checkForPosInt(input$n, minInt = 0), 
-    #        "n must be a positive integer > 10 and < 10^9"),
-    #   need(checkNumRange(input$R, minx = -1, maxx = 1, incEq = TRUE),
-    #        "R must be a value >= -1.0 and <= 1.0"),
-    #   need(checkNumRange(input$ci, minx = .69999, maxx = 1, incEq = FALSE),
-    #        "ci must be a value > .7 and < .99"),
-    #   need(checkForPosInt(input$dp, minInt = 1, maxInt = 5),
-    #        "dp must be a value between 1 and 5")
-    # )
+  RCI <- eventReactive(input$compute, {
+    validate(
+      need(input$SD, "You must give a usable value for SD"),
+      need(input$rel, "You must give a usable value for reliability"),
+      need(input$ci, "You must give a usable value for ci (usually .95)"),
+      need(input$dp, "You must give a usable value for dp (decimal places)")
+    )
     getRCI(input$SD,
-          input$rel,
-          input$ci,
-          input$dp)
+           input$rel,
+           input$ci,
+           input$dp)
+  })
+  
+  output$res <- renderText({
+    RCI()
   })
 }
 
