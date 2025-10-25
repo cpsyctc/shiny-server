3fd36330485003f9520a9d0a85b31d56cb24d3a9
Chris Evans <chris@psyctc.org>
2025-10-25 18:42:16 +0100

    Tweaks to CORE-OM_scoring2 so it's OK for public release and corresponding additions to index.html and to docs/CORE-OM/index.html to list it.

--- apps/CORE-OM_scoring2/app.R
+++ apps/CORE-OM_scoring2/app.R
@@ -1,14 +1,9 @@
 ### App that might help SystmOne (sic) users of the CORE-OM
+### app file name: CORE-OM_scoring2
 suppressMessages(library(shiny))
 suppressMessages(library(shinyWidgets))
 suppressMessages(library(tidyverse))
 suppressMessages(library(shiny.telemetry))
-
-### function
-getSvalFromPval <- function(pVal) {
-  sVal <- -log(pVal, base = 2)
-}
-
 
 ### 1. Initialize telemetry with default options (store to a local logfile)
 telemetry <- Telemetry$new(app_name = "CORE-OM_scoring2",
@@ -20,12 +15,13 @@ ui <- fluidPage(
   use_telemetry(), # 2. Add necessary Javascript to Shiny
   
   setBackgroundColor("#ffff99"),
-  h1(HTML("CORE-OM scores for SystmOne users\n\n")),
+  h1("CORE-OM scores for SystmOne users",
+     align = "center"),
   
-  p("I have just learned that the way that",
+  p("I learned recently that the way at least some ",
     a("SystmOne",
       href="https://en.wikipedia.org/wiki/SystmOne"),
-    "scores CORE-OM data is pretty unhelpful if people want a total score.  This app might help.  ",
+    "users see CORE-OM scores is pretty unhelpful if people want a total score.  This app might help.  ",
     "For now it assumes that the SystmOne screen returns domain scores as means ",
     "and this gives the correct total score in mean or 'clinical' scoring."),
   
@@ -73,10 +69,32 @@ ui <- fluidPage(
     ),
     
     mainPanel(
-      h3("Getting CORE total score from domain mean scores", align="center"),
+      h2("Getting CORE total score from domain mean scores", align="center"),
       verbatimTextOutput("res"),
-      p("This incredibly basic shiny app just gets total scores from mean domain scores."),
+      h3("Background to the scoring",
+         align = "center"),
+      p("This incredibly basic shiny app just gets overall (and non-risk) scores from the mean domain scores. ",
+        "You might think that these are just the mean of the domain scores but that's not the case because the ",
+        "domains have such different numbers of items so the correct process is the multiply those mean scores ",
+        "by the number of items in each domain, add them up and take the mean of all of them for the total score, ",
+        "(and just add up the those recreated totals for domains other than risk to get the non-risk score (doh!)  ",
+        "I have added the option to get those overall scores as 'clinical' scores, i.e. the mean scores multiplied ",
+        "by 10."),
+      br(),
+      p("This only works if all items have been completed on all domains but in at least one NHS Trust the way that ",
+        "SystmOne gets the responses is all or nothing so that assumption is safe.  ",
+        "If you are in the situation where SystmOne or some other software allows missing items and tells you domain ",
+        "mean scores and numbers of omitted (or completed) items you would need a different version of this app.  ",
+        "If you do want that, ",
+        a("contact me",
+        href="https://www.psyctc.org/psyctc/contact-me/"),
+        "."),
+      p("And, as you asked, yes I think it was a bad decision to have both the mean and the 'clinical' scoring ",
+        "methods but I didn't invent the 'clinical' one and both are now well established with the 'clinical' ",
+        "score probably being the most used for the CORE-10 now so we have to live with it!"),
       p(" "),
+      h3("General background and legalities",
+         align = "center"),
       p("App created by Chris Evans",
         a("PSYCTC.org",
           href="https://www.psyctc.org/psyctc/about-me/"),
